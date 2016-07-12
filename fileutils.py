cb2c4015b34560eb17246eddd1a49a75f0e09387
artem <artem.molchanov@vw.com>
2016-07-12 11:02:31 -0700

    initial commit

---
+++ fileutils.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import sys
+
+import fnmatch
+
+## Return the list of files in the designated directory with extension provided by the user
+# @param dir Input directory
+# @param ext Extension of the files
+def get_filelist_ext(dir, ext):
+    filelist = []
+    for file in os.listdir(dir):
+        if file.endswith(ext):
+            filelist.append(file)
+    return filelist
+
+## Tests functionality
+def main(argv):
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "dir",
+        help="Input directory"
+    )
+    parser.add_argument(
+        "ext",
+        help="Extension of files to filter"
+    )
+
+    args = parser.parse_args()
+
+    print get_filelist_ext(args.dir, args.ext)
+
+
+## Returns path,name,extension tuple from name
+# @param name Name you want to split
+# @param keepslash Keep slash at the end of the path or not
+def fileparts(name, keepslash=True):
+    e = os.path.splitext(name)[1][1:]
+    n = os.path.basename(os.path.splitext(name)[0])
+    p = os.path.dirname(name)
+
+    if p=='':
+        p = '.'
+
+    if keepslash:
+        p = p + '/'
+
+    return (p,n,e)
+
+
+
+def find_files_ext(dir, extensions):
+
+    ext_standard = []
+    for ext in extensions:
+        if ext[0] != '.':
+            ext = '.' + ext
+        ext_standard.append(ext)
+
+    matches = []
+    for root, dirs, files in os.walk(dir):
+        for filename in files:
+            if filename.endswith(ext_standard):
+                matches.append(filename)
+
+    return matches
+
+if __name__ == '__main__':
+    main(sys.argv)
+
+
